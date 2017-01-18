333458edde844447e66a92c80899082d8b4ce513
Josh Larson <jplhomer@gmail.com>
2017-01-17 21:02:14 -0600

    Insert example component

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Chatroom</title>
+
+        <link rel="stylesheet" href="css/app.css">
+    </head>
+    <body>
+        <div id="app">
+            <h1>Chatroom</h1>
+            <example></example>
+        </div>
+        <script src="js/app.js" charset="utf-8"></script>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/chat', function () {
+    return view('chat');
+});
